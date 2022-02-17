3c4858af6e3cb13f0a6ad417ec505d563df92a8d
tricky_coder <61200794+chaithra-ac@users.noreply.github.com>
2022-02-17 20:54:07 +0530

    Create CombinationSum.java

---
+++ CombinationSum.java
@@ -0,0 +1,24 @@
+/*
+https://leetcode.com/problems/combination-sum/
+*/
+class Solution {
+    public List<List<Integer>> combinationSum(int[] candidates, int target) {
+        List<List<Integer>>l=new ArrayList();
+        compute(l,new ArrayList(),candidates,target,0);
+        return l;
+    }
+    void compute(List<List<Integer>>l,List<Integer>cur,int[]a,int t,int n){
+        if(t<0||n==a.length){
+            return;
+        }
+        if(t==0){
+            l.add(new ArrayList(cur));
+            return;
+        }
+        cur.add(a[n]);
+        compute(l,cur,a,t-a[n],n);
+        cur.remove(cur.size()-1);
+        compute(l,cur,a,t,n+1);
+        
+    }
+}
