c10fbf3c86db2ed1e3806a10c64f853b8f6d8df5
tricky_coder <61200794+chaithra-ac@users.noreply.github.com>
2022-02-21 21:27:44 +0530

    Create PascalsTriangle.java

---
+++ PascalsTriangle.java
@@ -0,0 +1,21 @@
+/*
+https://leetcode.com/problems/pascals-triangle/
+*/
+class Solution {
+    List<List<Integer>>list=new ArrayList<>();
+    public List<List<Integer>> generate(int num) {
+        
+        for(int i=0;i<num;i++){
+            
+            list.add(new ArrayList());
+            list.get(i).add(1);
+            
+            for(int j=0;j<i-1;j++)
+                list.get(i).add(list.get(i-1).get(j)+list.get(i-1).get(j+1));
+            
+            if(i>0)list.get(i).add(1);
+                        // System.out.println(list);
+         } 
+        return list;
+     }
+}
