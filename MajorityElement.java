9b81d3e307ba302614eece9fe20024a3e22aa4ae
tricky_coder <61200794+chaithra-ac@users.noreply.github.com>
2022-02-21 11:03:45 +0530

    Create MajorityElement.java

---
+++ MajorityElement.java
@@ -0,0 +1,18 @@
+/*
+https://leetcode.com/problems/majority-element/
+*/
+
+
+
+
+class Solution {
+    public int majorityElement(int[] nums) {
+        int res=0,count=0;
+       for(int i:nums){
+         if(count==0)
+             res=i;
+           count+=(res==i)?+1:-1;
+       } 
+        return res;
+    }
+}
