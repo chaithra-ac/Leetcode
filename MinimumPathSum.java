54f8e97de85f72097b555b15f35077b4e7b302be
tricky_coder <61200794+chaithra-ac@users.noreply.github.com>
2022-02-23 11:34:00 +0530

    Create MinimumPathSum.java

---
+++ MinimumPathSum.java
@@ -0,0 +1,28 @@
+/*
+https://leetcode.com/problems/minimum-path-sum/
+*/
+class Solution {
+    int min=Integer.MAX_VALUE;
+    int [][]dp;
+    public int minPathSum(int[][] g) {
+        dp=new int[g.length][g[0].length];
+    
+      return compute(g,g.length,g[0].length,0,0,0); 
+    }
+    int compute(int[][]g,int m,int n,int x,int y,int res){
+        for(int i=0;i<m;i++){
+            for(int j=0;j<n;j++){
+               if(i==0){
+                   dp[i][j]=g[i][j];
+                   if(j>0)dp[i][j]=g[i][j]+dp[i][j-1];
+               }
+                    
+                else if(j==0)
+                    dp[i][j]=g[i][j]+dp[i-1][j];
+                else 
+                    dp[i][j]=g[i][j]+Math.min(dp[i][j-1],dp[i-1][j]);
+            }
+        }
+      return dp[m-1][n-1];   
+    }
+}
