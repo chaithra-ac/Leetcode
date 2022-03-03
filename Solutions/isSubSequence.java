f3d56337d52b33d791bece56396318c947950553
tricky_coder <61200794+chaithra-ac@users.noreply.github.com>
2022-03-03 22:07:53 +0530

    Create isSubSequence.java

---
+++ Solutions/isSubSequence.java
@@ -0,0 +1,11 @@
+class Solution {
+    public boolean isSubsequence(String s, String t) {
+     int st=0,end=t.length(),mid=0,j=0;
+        while(st<end){
+            mid=s+(end-st)/2;
+            if(s.charAt(j)==s.charAt(mid))
+                return true;
+            else if()
+        }
+    }
+}
