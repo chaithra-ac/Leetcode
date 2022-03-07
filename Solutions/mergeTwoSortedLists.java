9a14a9815fd898c533a3a60eec431640f7209a2d
tricky_coder <61200794+chaithra-ac@users.noreply.github.com>
2022-03-07 11:09:42 +0530

    Create mergeTwoSortedLists.java

---
+++ Solutions/mergeTwoSortedLists.java
@@ -0,0 +1,69 @@
+//recursive approach
+/**
+ * Definition for singly-linked list.
+ * public class ListNode {
+ *     int val;
+ *     ListNode next;
+ *     ListNode() {}
+ *     ListNode(int val) { this.val = val; }
+ *     ListNode(int val, ListNode next) { this.val = val; this.next = next; }
+ * }
+ */
+class Solution {
+    public ListNode mergeTwoLists(ListNode l1, ListNode l2) {
+               if(l1==null){
+            return l2;
+        }
+        if(l2==null){
+            return l1;
+        }
+        if(l1.val<=l2.val){
+            l1.next=mergeTwoLists(l1.next,l2);
+            return l1;
+        }
+        else{
+            l2.next=mergeTwoLists(l1,l2.next);
+            return l2;
+        }
+ 
+    }
+}
+
+
+// iterative approach
+/**
+ * Definition for singly-linked list.
+ * public class ListNode {
+ *     int val;
+ *     ListNode next;
+ *     ListNode() {}
+ *     ListNode(int val) { this.val = val; }
+ *     ListNode(int val, ListNode next) { this.val = val; this.next = next; }
+ * }
+ */
+class Solution {
+    public ListNode mergeTwoLists(ListNode l1, ListNode l2) {
+        ListNode res=new ListNode(0);
+        ListNode du=res;
+        while(l1!=null||l2!=null){
+            if(l1==null){
+                res.next=l2;
+                l2=l2.next;
+            }
+            else if(l2==null){
+                res.next=l1;
+                l1=l1.next;
+            }
+            else if(l1.val<l2.val){
+                res.next=l1;
+                l1=l1.next;
+            }
+            else {
+                res.next=l2;
+                l2=l2.next;
+            }
+            res=res.next;
+        }
+        return du.next;
+    }
+}
