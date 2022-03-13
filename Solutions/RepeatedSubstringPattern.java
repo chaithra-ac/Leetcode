d4e8d97f4b8523ea541dabbca57c36515cabee00
tricky_coder <61200794+chaithra-ac@users.noreply.github.com>
2022-03-13 15:20:44 +0530

    Problem Numbee(459)

---
+++ Solutions/RepeatedSubstringPattern.java
@@ -0,0 +1,10 @@
+// https://leetcode.com/problems/repeated-substring-pattern/
+class Solution {
+        
+        public boolean repeatedSubstringPattern(String s) {
+        String str = s+s;
+                // System.out.print(s.substring(1,s.length()-1));
+        return str.substring(1, str.length() - 1).contains(s);
+    }
+
+}
