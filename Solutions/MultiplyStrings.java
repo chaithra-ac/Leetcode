fe7897fe2daae2001cb86f01e509115ea33095fb
tricky_coder <61200794+chaithra-ac@users.noreply.github.com>
2022-03-15 17:34:20 +0530

    problem no (43)

--- Solutions/MultiplyStrings.java
+++ Solutions/MultiplyStrings.java
@@ -1,11 +1,40 @@
 //https://leetcode.com/problems/multiply-strings/
-import java.math.BigInteger;
-
 class Solution {
     public String multiply(String num1, String num2) {
-        BigInteger n1=new BigInteger(num1);
-        BigInteger n2=new BigInteger(num2);
-        n1=n1.multiply(n2);        
-        return ""+n1;
+        char[] num1s = num1.toCharArray();
+        char[] num2s = num2.toCharArray();
+        if(num1s[0] == '0' || num2s[0] == '0') return "0";
+
+        StringBuilder sb = new StringBuilder();
+        int startPos = 0;
+        int carry = 0;
+        
+        for(int i = num2s.length - 1; i >= 0; i--) {
+            int n2 = num2s[i] - '0';
+            int curPos = startPos;
+            for(int j = num1s.length - 1; j >= 0;j--) {
+                int n1 = num1s[j] - '0';
+                if(curPos >= sb.length()) sb.append('0');
+                int multiply = sb.charAt(curPos) - '0' + n1*n2;
+                char digit = (char)(multiply % 10 + '0');
+                sb.setCharAt(curPos, digit);
+                carry = multiply /10;
+                curPos++;
+                // Add to the next position this can also help with the case we have carry left in the end
+                // it might even be a digit since it might exceed 10
+                if(carry > 0) {
+                    if(curPos >= sb.length()) {
+                        sb.append((char)(carry + '0'));
+                    } else {
+                        sb.setCharAt(curPos, (char)(carry + sb.charAt(curPos)));     
+                    }
+                }
+            }
+            startPos++;
+        }
+        
+        sb.reverse();
+        
+        return sb.toString();
     }
 }
