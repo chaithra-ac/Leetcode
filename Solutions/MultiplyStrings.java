d89e4d0bafb5ee563a65fe18452728471bf096f6
tricky_coder <61200794+chaithra-ac@users.noreply.github.com>
2022-03-15 17:27:46 +0530

    problem no (43)

---
+++ Solutions/MultiplyStrings.java
@@ -0,0 +1,11 @@
+//https://leetcode.com/problems/multiply-strings/
+import java.math.BigInteger;
+
+class Solution {
+    public String multiply(String num1, String num2) {
+        BigInteger n1=new BigInteger(num1);
+        BigInteger n2=new BigInteger(num2);
+        n1=n1.multiply(n2);        
+        return ""+n1;
+    }
+}
