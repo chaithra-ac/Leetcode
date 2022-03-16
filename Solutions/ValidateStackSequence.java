a9c0a83f58b06720d8ff7b566418a806ce744681
tricky_coder <61200794+chaithra-ac@users.noreply.github.com>
2022-03-16 09:03:43 +0530

    problem number (946)

---
+++ Solutions/ValidateStackSequence.java
@@ -0,0 +1,23 @@
+//https://leetcode.com/problems/validate-stack-sequences/
+class Solution {
+    public boolean validateStackSequences(int[] pushed, int[] popped) {
+     Stack<Integer>stack=new Stack<>();
+       int  j=0,i=1,n=popped.length;
+        stack.push(pushed[0]);
+     while(j<n){
+         // System.out.println(stack+" "+i+" "+j);
+         
+       if(!stack.isEmpty()&&stack.peek()==popped[j]){
+          stack.pop();
+          j++;
+       }
+       else if(i<n)
+          stack.push(pushed[i++]);
+            
+       else
+          return false;
+         
+     }
+     return j==n;
+    }
+}
