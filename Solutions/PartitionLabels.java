dee1e3b1ac9d8a1d6f884b1a97c8fc35a900ad6a
tricky_coder <61200794+chaithra-ac@users.noreply.github.com>
2022-03-22 10:51:46 +0530

    P  N (763)

---
+++ Solutions/PartitionLabels.java
@@ -0,0 +1,24 @@
+//https://leetcode.com/problems/partition-labels/
+
+class Solution {
+    HashMap<Character,Integer>map=new HashMap<>(26);
+    List<Integer>list=new ArrayList<>();
+    public List<Integer> partitionLabels(String s) {
+        
+      int start=0,max=0;
+    
+      for(int i=0;i<s.length();i++)
+          map.put(s.charAt(i),i);
+        
+        for(int i=0;i<s.length();i++){
+            
+           max=Math.max(max,map.get(s.charAt(i))); 
+            if(max==i){
+                list.add(i+1-start);
+                start=i+1;
+            }
+        }
+        
+     return list;   
+    }
+}
