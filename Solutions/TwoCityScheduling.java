4e76eb7ef52e773604e73b37a294dce6e8ae865e
tricky_coder <61200794+chaithra-ac@users.noreply.github.com>
2022-03-25 11:59:32 +0530

    1029

---
+++ Solutions/TwoCityScheduling.java
@@ -0,0 +1,20 @@
+//https://leetcode.com/problems/two-city-scheduling/
+
+
+class Solution {
+    public int twoCitySchedCost(int[][] costs) {
+        int n = costs.length;
+        int[] dif = new int[n];
+        int min = 0, i= 0;
+        for(int[] cost : costs){
+            dif[i++] = cost[1] - cost[0];
+            min += cost[0];
+        }
+        Arrays.sort(dif);
+        
+        for( i = 0; i < n/2; i++){
+            min += dif[i];
+        }
+        return min;
+    }
+}
