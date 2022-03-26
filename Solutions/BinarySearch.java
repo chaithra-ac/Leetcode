904b1f707ce102be955708e984605bfcf60a945e
tricky_coder <61200794+chaithra-ac@users.noreply.github.com>
2022-03-26 10:09:02 +0530

    Problem number (704)

---
+++ Solutions/BinarySearch.java
@@ -0,0 +1,15 @@
+//https://leetcode.com/problems/binary-search/
+
+class Solution {
+    public int search(int[] nums, int target) {
+      int s=0,e=nums.length,mid=0;
+        while(s<e){
+           mid=s+(e-s)/2;
+            if(nums[mid]==target)return mid;
+            else if(nums[mid]<target)
+                s=mid+1;
+            else e=mid;
+        }
+        return -1;
+    }
+}
