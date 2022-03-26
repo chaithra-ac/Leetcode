018b64060694b046e93abe96aae06777a686fb6d
tricky_coder <61200794+chaithra-ac@users.noreply.github.com>
2022-03-26 11:10:07 +0530

    Problem number (58)

---
+++ Solutions/LengthOfLastWord.java
@@ -0,0 +1,9 @@
+//https://leetcode.com/problems/length-of-last-word/
+
+
+class Solution {
+    public int lengthOfLastWord(String s) {
+     String []sa=s.split(" ");
+        return sa[sa.length-1].length();
+    }
+}
