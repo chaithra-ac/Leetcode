323d6dec3c1f0d6e0526b375ec8109e73154d275
tricky_coder <61200794+chaithra-ac@users.noreply.github.com>
2022-03-29 10:18:05 +0530

    Create 287-FindTheDuplicateNumber.java

---
+++ Solutions/287-FindTheDuplicateNumber.java
@@ -0,0 +1,12 @@
+// https://leetcode.com/problems/find-the-duplicate-number/
+
+class Solution {
+    public int findDuplicate(int[] nums) {
+     for(int i=0;i<nums.length;i++){
+         int c=Math.abs(nums[i]);
+         if(nums[c]<0)return c;
+         else nums[c]*=-1;
+     }
+        return -1;
+    }
+}
