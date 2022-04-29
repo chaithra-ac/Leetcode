09864832a27d505f54ed49c89e6e0d6fe9b9dbf8
tricky_coder <61200794+chaithra-ac@users.noreply.github.com>
2022-04-29 18:48:48 +0530

    Create 278-First Bad Version.java

---
+++ Solutions/278-First Bad Version.java
@@ -0,0 +1,18 @@
+//https://leetcode.com/problems/first-bad-version/
+/* The isBadVersion API is defined in the parent class VersionControl.
+      boolean isBadVersion(int version); */
+
+public class Solution extends VersionControl {
+    public int firstBadVersion(int n) {
+     int s=1,mid=0,e=n;
+        
+        while(s<e){
+            mid=s+(e-s)/2;
+           if(isBadVersion(mid))
+                e=mid;
+            else
+                s=mid+1;
+        }
+    return s;
+    }
+}
