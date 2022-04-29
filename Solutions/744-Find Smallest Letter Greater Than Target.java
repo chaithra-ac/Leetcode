cd45dc3c5a25bf9251f1aac3d96e0000dfb3f44c
tricky_coder <61200794+chaithra-ac@users.noreply.github.com>
2022-04-29 18:27:25 +0530

    Create 744-Find Smallest Letter Greater Than Target.java

---
+++ Solutions/744-Find Smallest Letter Greater Than Target.java
@@ -0,0 +1,15 @@
+//https://leetcode.com/problems/find-smallest-letter-greater-than-target/
+class Solution {
+    public char nextGreatestLetter(char[] l, char target) {
+     int s=0,e=l.length,mid=0;
+        while(s<e){
+         mid=s+(e-s)/2;
+             if(l[mid]>target)e=mid;
+                else{
+                   s=mid+1; 
+                }
+        }
+        // System.out.print(l[s]+" "+l[mid]+" "+l[e]);
+        return l[s%l.length];
+    }
+}
