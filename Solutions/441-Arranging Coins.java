145b645fa9a90f6ede1a72c6eaf66340df45477d
tricky_coder <61200794+chaithra-ac@users.noreply.github.com>
2022-04-30 21:09:49 +0530

    Create 441-Arranging Coins.java

---
+++ Solutions/441-Arranging Coins.java
@@ -0,0 +1,12 @@
+//https://leetcode.com/problems/arranging-coins/
+class Solution {
+    public int arrangeCoins(int n) {
+      int s=1,i=1,r=0;
+        while((n-i)>=0){
+            r++;
+            n-=(i++);
+        }
+        // System.out.println(Integer.MAX_VALUE);
+        return r;
+    }
+}
