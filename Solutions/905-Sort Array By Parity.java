3764b4725c070a313108b015313b503d37247e2f
tricky_coder <61200794+chaithra-ac@users.noreply.github.com>
2022-05-02 12:53:57 +0530

    Create 905-Sort Array By Parity.java

---
+++ Solutions/905-Sort Array By Parity.java
@@ -0,0 +1,15 @@
+//https://leetcode.com/problems/sort-array-by-parity/
+class Solution {
+    public int[] sortArrayByParity(int[] nums) {
+       int i=0,j=0;
+        while(i<nums.length){
+            if(nums[i]%2==0){
+                int t=nums[i];
+                nums[i]=nums[j];
+                nums[j++]=t;
+            }
+            i++;
+        }
+        return nums;
+    }
+}
