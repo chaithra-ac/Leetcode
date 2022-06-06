5a42dd3210081851eae761a1d4e362e7ab0d16c2
tricky_coder <61200794+chaithra-ac@users.noreply.github.com>
2022-06-06 17:46:43 +0530

    Update 160-IntersectionofTwoLinkedLists.java

--- Solutions/160-IntersectionofTwoLinkedLists.java
+++ Solutions/160-IntersectionofTwoLinkedLists.java
@@ -10,6 +10,31 @@
  *     }
  * }
  */
+//approach 1
+public class Solution {
+    public ListNode getIntersectionNode(ListNode headA, ListNode headB) {
+    HashSet<ListNode>set=new HashSet<>();
+        while(headA!=null||headB!=null){
+            if(headA!=null){
+            if(set.contains(headA))
+                return headA;
+            set.add(headA);
+            headA=headA.next;
+            }
+            if(headB!=null){
+            if(set.contains(headB))
+            return headB;
+            set.add(headB);
+            headB=headB.next;
+            }
+            
+        }
+       return null; 
+    }
+}
+
+
+//approach 2
 public class Solution {
     public ListNode getIntersectionNode(ListNode headA, ListNode headB) {
        int la=length(headA); 
