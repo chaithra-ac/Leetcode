32568d04120f711ef27f546f46d880786962220a
tricky_coder <61200794+chaithra-ac@users.noreply.github.com>
2022-06-07 22:13:30 +0530

    Create 1961. Check If String Is a Prefix of Array.java

---
+++ Solutions/1961. Check If String Is a Prefix of Array.java
@@ -0,0 +1,13 @@
+//https://leetcode.com/problems/check-if-string-is-a-prefix-of-array/
+class Solution {
+    public boolean isPrefixString(String s, String[] words) {
+        StringBuffer sb=new StringBuffer();
+      for(int i=0;i<words.length;i++){
+          sb.append(words[i]);
+          if(sb.length()>s.length())
+              return false;
+          else if(s.length()==sb.length()&&s.equals(sb.toString()))return true;
+      } 
+        return false;
+    }
+}
