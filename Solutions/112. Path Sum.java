643f23efc40faccad05f7beb3cd53c0bb36c724b
tricky_coder <61200794+chaithra-ac@users.noreply.github.com>
2022-06-18 22:33:36 +0530

    Create 112. Path Sum.java

---
+++ Solutions/112. Path Sum.java
@@ -0,0 +1,24 @@
+//https://leetcode.com/problems/path-sum/
+/**
+ * Definition for a binary tree node.
+ * public class TreeNode {
+ *     int val;
+ *     TreeNode left;
+ *     TreeNode right;
+ *     TreeNode() {}
+ *     TreeNode(int val) { this.val = val; }
+ *     TreeNode(int val, TreeNode left, TreeNode right) {
+ *         this.val = val;
+ *         this.left = left;
+ *         this.right = right;
+ *     }
+ * }
+ */
+class Solution {
+    public boolean hasPathSum(TreeNode root, int target) {
+   if(root==null)return false;
+        if(target-root.val==0&&root.left==null&&root.right==null)return true;
+        
+    return  hasPathSum(root.left,target-root.val)||hasPathSum(root.right,target-root.val);  
+    }
+}
