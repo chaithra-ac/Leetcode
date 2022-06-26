384c637cd55dec87732f9f15dae10372685ef9ef
tricky_coder <61200794+chaithra-ac@users.noreply.github.com>
2022-06-26 12:03:00 +0530

    Create 2319. Check if Matrix Is X-Matrix.java

---
+++ Solutions/2319. Check if Matrix Is X-Matrix.java
@@ -0,0 +1,14 @@
+// https://leetcode.com/problems/check-if-matrix-is-x-matrix/
+class Solution {
+    public boolean checkXMatrix(int[][] g) {
+        int op=g.length-1,n=g.length;
+      for(int i=0;i<n;i++){
+          for(int j=0;j<n;j++){
+              if((j==op||i==j)&&g[i][j]<=0)return false;
+              if(j!=op&&i!=j&&g[i][j]>0)return false;
+          }
+          op--;
+      }
+      return true; 
+    }
+}
