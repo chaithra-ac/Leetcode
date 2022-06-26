50987b24e5e732a91bcaaeddd6c2ebb227693c5c
tricky_coder <61200794+chaithra-ac@users.noreply.github.com>
2022-06-26 12:12:10 +0530

    Update 2319. Check if Matrix Is X-Matrix.java

--- Solutions/2319. Check if Matrix Is X-Matrix.java
+++ Solutions/2319. Check if Matrix Is X-Matrix.java
@@ -12,3 +12,21 @@ class Solution {
       return true; 
     }
 }
+
+
+
+
+class Solution {
+    public boolean checkXMatrix(int[][] g) {
+        int op=g.length-1,n=g.length;
+      for(int i=0;i<n;i++){
+          for(int j=0;j<n;j++){
+              System.out.print(g[i][j]);
+              if(j==op||i==j){if(g[i][j]==0)return false;}
+              else if(g[i][j]>0)return false;
+          }
+          op--;
+      }
+      return true; 
+    }
+}
