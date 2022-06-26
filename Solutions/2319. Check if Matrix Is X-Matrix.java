700aed885214a18671394281d56992e79e091f14
tricky_coder <61200794+chaithra-ac@users.noreply.github.com>
2022-06-26 12:09:45 +0530

    Update 2319. Check if Matrix Is X-Matrix.java

--- Solutions/2319. Check if Matrix Is X-Matrix.java
+++ Solutions/2319. Check if Matrix Is X-Matrix.java
@@ -4,7 +4,7 @@ class Solution {
         int op=g.length-1,n=g.length;
       for(int i=0;i<n;i++){
           for(int j=0;j<n;j++){
-              if((j==op||i==j)&&g[i][j]<=0)return false;
+              if((j==op||i==j)&&g[i][j]==0)return false;
               if(j!=op&&i!=j&&g[i][j]>0)return false;
           }
           op--;
