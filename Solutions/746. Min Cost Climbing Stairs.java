dcfd3d2c4ed3d51feee95b7a38c364f4d1a1fb66
tricky_coder <61200794+chaithra-ac@users.noreply.github.com>
2022-06-28 13:36:22 +0530

    Update 746. Min Cost Climbing Stairs.java

--- Solutions/746. Min Cost Climbing Stairs.java
+++ Solutions/746. Min Cost Climbing Stairs.java
@@ -1,11 +1,50 @@
 //https://leetcode.com/problems/min-cost-climbing-stairs/
+
+//dp
 class Solution {
-    public int minCostClimbingStairs(int[] cost) {
-        int[]dp=new int[cost.length+1]; 
-        int n=cost.length;
-        for (int i = 2; i <= n; ++i) {
-          dp[i] = Math.min(dp[i - 1] + cost[i - 1], dp[i - 2] + cost[i - 2]);
-  }
-  return dp[n];
+    int dp[];
+    public int climbStairs(int n) {
+        dp=new int[n+1];
+        dp[0]=dp[1]=1;
+        for(int i=2;i<=n;i++){
+            dp[i]=dp[i-1]+dp[i-2];
+        }
+        System.out.print(Arrays.toString(dp));
+        return dp[n];
     }
-  }
+}
+
+
+//memoization
+class Solution {
+    int sum=0;
+    int dp[];
+    public int climbStairs(int n) {
+        dp=new int[n+1];
+       compute(n);
+        // System.out.print(Arrays.toString(dp));
+        return dp[n];
+    }
+    int compute(int n){
+       if(n==0)
+           return 1;
+        if(n<0)return 0;
+        if(dp[n]!=0)return dp[n];
+        return dp[n]=compute(n-1)+compute(n-2);
+    }
+}
+
+
+
+// recursion [TLE]
+class Solution {
+    public int climbStairs(int n) {
+       return compute(n);
+    }
+    int compute(int n){
+       if(n==0)
+           return 1;
+        if(n<0)return 0;
+        return compute(n-1)+compute(n-2);
+    }
+}
