27c2b01667b4b5c8d03444fcd5e308c58fa0cfda
tricky_coder <61200794+chaithra-ac@users.noreply.github.com>
2022-06-28 14:08:08 +0530

    Update 746. Min Cost Climbing Stairs.java

--- Solutions/746. Min Cost Climbing Stairs.java
+++ Solutions/746. Min Cost Climbing Stairs.java
@@ -8,10 +8,11 @@ class Solution {
     public int minCostClimbingStairs(int[] cost) {
         int[]dp=new int[cost.length+1]; 
         int n=cost.length;
-        for (int i = 2; i <= n; ++i) {
-          dp[i] = Math.min(dp[i - 1] + cost[i - 1], dp[i - 2] + cost[i - 2]);
-  }
-  return dp[n];
+	    	for (int i=0; i<n; i++) {
+		if (i<2) dp[i] = cost[i];
+		else dp[i] = cost[i] + Math.min(dp[i-1], dp[i-2]);
+	}
+      return Math.min(dp[n-1], dp[n-2]);
     }
   }
 
