04befc95849a943f57fe44df11595ddf530b8f75
tricky_coder <61200794+chaithra-ac@users.noreply.github.com>
2022-07-02 21:44:21 +0530

    Create 86. Partition List.java

---
+++ Solutions/86. Partition List.java
@@ -0,0 +1,33 @@
+// https://leetcode.com/problems/partition-list/
+/**
+ * Definition for singly-linked list.
+ * public class ListNode {
+ *     int val;
+ *     ListNode next;
+ *     ListNode() {}
+ *     ListNode(int val) { this.val = val; }
+ *     ListNode(int val, ListNode next) { this.val = val; this.next = next; }
+ * }
+ */
+class Solution {
+    public ListNode partition(ListNode head, int x) {
+    ListNode dummyl=new ListNode(0);
+    ListNode less=dummyl;
+    ListNode dummyg=new ListNode(0);
+    ListNode great=dummyg;
+       while(head!=null){
+           if(head.val<x){
+               dummyl.next=head;
+               dummyl=dummyl.next;
+           }
+           else{
+               dummyg.next=head;
+              dummyg=dummyg.next;
+               }
+               head=head.next;
+       }
+        dummyg.next=null;
+        dummyl.next=great.next;
+        return less.next;
+    }
+}
