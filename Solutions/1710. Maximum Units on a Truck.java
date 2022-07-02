74007d8e20cc38307ceb63f14d6445afad39c48f
tricky_coder <61200794+chaithra-ac@users.noreply.github.com>
2022-07-02 20:38:59 +0530

    Create 1710. Maximum Units on a Truck.java

---
+++ Solutions/1710. Maximum Units on a Truck.java
@@ -0,0 +1,15 @@
+//https://leetcode.com/problems/maximum-units-on-a-truck/
+class Solution {
+    public int maximumUnits(int[][] boxTypes, int truckSize) {
+  Arrays.sort(boxTypes, (a, b) -> Integer.compare(b[1], a[1]));
+    int res=0,cur=0;
+    for(int box[] : boxTypes){
+        cur = Math.min(truckSize, box[0]);
+        res += cur * box[1];
+        if((truckSize -= cur) == 0)
+            return res;
+    }
+    return res;
+  
+    }
+}
