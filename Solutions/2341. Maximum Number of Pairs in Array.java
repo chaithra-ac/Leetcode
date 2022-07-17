b852cdce7d7fe61360aedded04fbd64e450099ef
tricky_coder <61200794+chaithra-ac@users.noreply.github.com>
2022-07-17 19:37:57 +0530

    Create 2341. Maximum Number of Pairs in Array.java

---
+++ Solutions/2341. Maximum Number of Pairs in Array.java
@@ -0,0 +1,16 @@
+// https://leetcode.com/problems/maximum-number-of-pairs-in-array/
+
+
+class Solution {
+    public int[] numberOfPairs(int[] nums) {
+      HashMap<Integer,Integer>map=new HashMap<>();
+        for(int i=0;i<nums.length;i++)
+            map.put(nums[i],map.getOrDefault(nums[i],0)+1);
+        int l=0,p=0;
+        for(int i:map.keySet()){
+            p+=map.get(i)/2;
+            l+=map.get(i)%2; 
+        }
+        return new int[]{p,l};
+    }
+}
