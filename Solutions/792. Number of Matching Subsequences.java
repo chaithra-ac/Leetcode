6e78ed3a3d825e7756c65e31818ac03e5a13b903
tricky_coder <61200794+chaithra-ac@users.noreply.github.com>
2022-07-21 00:17:27 +0530

    Create 792. Number of Matching Subsequences.java

---
+++ Solutions/792. Number of Matching Subsequences.java
@@ -0,0 +1,33 @@
+// https://leetcode.com/problems/number-of-matching-subsequences/
+class Solution {
+    public int numMatchingSubseq(String s, String[] words) {
+      HashMap<Character,ArrayList<Integer>>map=new HashMap<>();
+        
+       for(int i=0;i<s.length();i++){
+           if(map.containsKey(s.charAt(i)))
+           map.get(s.charAt(i)).add(i);
+        else {
+            map.put(s.charAt(i),new ArrayList<Integer>());
+            map.get(s.charAt(i)).add(i);
+            }
+       }
+        int count=0;
+        for(int i=0;i<words.length;i++){
+            String w=words[i];
+            int j=0,pre=-1;
+            while(j<w.length()){
+                int f=0;
+                if(map.containsKey(w.charAt(j))){
+                  for(int k:map.get(w.charAt(j))){
+                      if(pre<k){
+                          pre=k;f=1;
+                          break;
+                      }}}
+                if(j==w.length()-1)count++;
+                if(f==0)break;
+                j++;
+            }
+        }
+           return count;
+    }
+}
