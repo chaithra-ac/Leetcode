6612ec4ea91e506cf0090a9ad6b621a2603aee5e
tricky_coder <61200794+chaithra-ac@users.noreply.github.com>
2022-07-21 17:10:31 +0530

    Update 657. Robot Return to Origin.java

--- Solutions/657. Robot Return to Origin.java
+++ Solutions/657. Robot Return to Origin.java
@@ -1,5 +1,6 @@
 // https://leetcode.com/problems/robot-return-to-origin/
 
+//approach 1
 class Solution {
     public boolean judgeCircle(String moves) {
        HashMap<Character,ArrayList<Integer>>map=new HashMap<>();
@@ -12,6 +13,23 @@ class Solution {
         for(int k=0;k<moves.length();k++){
           i+=map.get(moves.charAt(k)).get(0);
           j+=map.get(moves.charAt(k)).get(1);
+        }
+        return i==0&&j==0;
+
+    }
+}
+
+
+//approach 2
+class Solution {
+    public boolean judgeCircle(String moves) {
+         int i=0,j=0;       
+        for(int k=0;k<moves.length();k++){
+          if(moves.charAt(k)=='U')i-=1;
+          else if(moves.charAt(k)=='D')i+=1;
+          else if(moves.charAt(k)=='L') j-=1;
+          else j+=1;
+              
         }
         return i==0&&j==0;
 
