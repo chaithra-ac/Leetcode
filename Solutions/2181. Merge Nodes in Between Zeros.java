1bc742708dd61a054de1a8579d40f40efe9a0c43
tricky_coder <61200794+chaithra-ac@users.noreply.github.com>
2022-07-22 10:04:45 +0530

    Update and rename 2181. Merge Nodes in Between Zeros . java to 2181. Merge Nodes in Between Zeros.java

--- Solutions/2181. Merge Nodes in Between Zeros.java
+++ Solutions/2181. Merge Nodes in Between Zeros.java
@@ -28,3 +28,35 @@ class Solution {
         return res.next;
     }
 }
+
+
+//approach 2
+/**
+ * Definition for singly-linked list.
+ * public class ListNode {
+ *     int val;
+ *     ListNode next;
+ *     ListNode() {}
+ *     ListNode(int val) { this.val = val; }
+ *     ListNode(int val, ListNode next) { this.val = val; this.next = next; }
+ * }
+ */
+ 
+class Solution {
+    ListNode dummy=new ListNode(0);
+    ListNode res=dummy;
+    public ListNode mergeNodes(ListNode head) {
+      head=head.next;
+     while(head!=null){
+         if(head.next==null)return res;
+         if(head.val==0){
+             dummy.next=new ListNode(0);
+             dummy=dummy.next;
+         }
+         else
+         dummy.val+=head.val;
+         head=head.next;
+     }
+        return null;
+    }
+}
