41055677df9cadb2e3bc746226815bf6cf233cc4
tricky_coder <61200794+chaithra-ac@users.noreply.github.com>
2022-07-24 12:05:59 +0530

    Create 2351. First Letter to Appear Twice .java

---
+++ Solutions/2351. First Letter to Appear Twice .java
@@ -0,0 +1,11 @@
+// https://leetcode.com/problems/first-letter-to-appear-twice/
+class Solution {
+    public char repeatedCharacter(String s) {
+       HashMap<Character,Integer>map=new HashMap<>();
+        for(int i=0;i<s.length();i++){
+            if(map.containsKey(s.charAt(i)))return s.charAt(i);
+            else map.put(s.charAt(i),1);
+        }
+        return ' ';
+    }
+}
