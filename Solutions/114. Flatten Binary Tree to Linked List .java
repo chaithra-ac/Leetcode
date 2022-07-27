9b01b6a556f3cb8061f7301a2d66924323b62d66
tricky_coder <61200794+chaithra-ac@users.noreply.github.com>
2022-07-27 10:09:40 +0530

    Update 114. Flatten Binary Tree to Linked List .java

--- Solutions/114. Flatten Binary Tree to Linked List .java
+++ Solutions/114. Flatten Binary Tree to Linked List .java
@@ -34,3 +34,17 @@ class Solution {
         flatten(root.right);
     }
 }
+
+
+///approach-2
+private TreeNode prev = null;
+
+public void flatten(TreeNode root) {
+    if (root == null)
+        return;
+    flatten(root.right);
+    flatten(root.left);
+    root.right = prev;
+    root.left = null;
+    prev = root;
+}
