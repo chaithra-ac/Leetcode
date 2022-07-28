4c07892bb45d6281facb14986fecfbd257b7cb4f
tricky_coder <61200794+chaithra-ac@users.noreply.github.com>
2022-07-28 21:40:59 +0530

    Create 242. Valid Anagram .java

---
+++ Solutions/242. Valid Anagram .java
@@ -0,0 +1,22 @@
+// https://leetcode.com/problems/valid-anagram/
+class Solution {
+    public boolean isAnagram(String s, String t) {
+     HashMap<Character,Integer>map=new HashMap<>();
+        
+        if(s.length()!=t.length())return false;
+        
+        for(int i=0;i<s.length();i++)
+            map.put(s.charAt(i),map.getOrDefault(s.charAt(i),0)+1);
+        
+        for(int i=0;i<s.length();i++){
+             int temp=map.containsKey(t.charAt(i))?map.get(t.charAt(i)):0;
+              if(temp>0){
+               map.put(t.charAt(i),temp-1);
+              }
+            else return false;
+        }
+        for(int i:map.values())
+            if(i>0)return false;
+        return true;
+    }
+}
