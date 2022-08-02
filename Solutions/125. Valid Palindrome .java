c80a3e4dad6f7225b0dd0ff7de9707662839574b
tricky_coder <61200794+chaithra-ac@users.noreply.github.com>
2022-08-02 19:51:13 +0530

    Update 125. Valid Palindrome .java

--- Solutions/125. Valid Palindrome .java
+++ Solutions/125. Valid Palindrome .java
@@ -4,8 +4,7 @@ class Solution {
     public boolean isPalindrome(String s) {
       String format=s.toLowerCase();
         format=format.replaceAll("[^a-z0-9]", "");
-        StringBuilder sb=new StringBuilder(st);
-        return st.equals(sb.reverse().toString());
-              
+        StringBuilder sb=new StringBuilder(format);
+        return format.equals(sb.reverse().toString());         
     }
 }
