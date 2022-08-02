ac00d4ff50c890595d1ef87d8528ba6a89895c1c
tricky_coder <61200794+chaithra-ac@users.noreply.github.com>
2022-08-02 10:46:26 +0530

    Create 841. Keys and Rooms .java

---
+++ Solutions/841. Keys and Rooms .java
@@ -0,0 +1,22 @@
+// https://leetcode.com/problems/keys-and-rooms/
+class Solution {
+    public boolean canVisitAllRooms(List<List<Integer>> rooms) {
+        boolean[]vis=new boolean[rooms.size()];
+        Queue<Integer>q=new LinkedList<>();
+        q.add(0);
+        vis[0]=true;
+        int count=1;
+        while(!q.isEmpty()){
+            int cur=q.poll();
+            for(int adj:rooms.get(cur)){
+                if(!vis[adj]){
+                    count++;
+                    vis[adj]=true;
+                    q.add(adj);
+                }
+            }
+        }
+        // for(boolean i:vis)if(!i)return false;
+        return count==rooms.size();
+    }
+}
