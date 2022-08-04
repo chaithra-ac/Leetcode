6c2896cd26c4789d64a931e389a04c461241b7f6
tricky_coder <61200794+chaithra-ac@users.noreply.github.com>
2022-08-04 07:52:53 +0530

    Update 318. Maximum Product of Word Lengths .java

--- Solutions/318. Maximum Product of Word Lengths .java
+++ Solutions/318. Maximum Product of Word Lengths .java
@@ -1,5 +1,29 @@
 // https://leetcode.com/problems/maximum-product-of-word-lengths/
 
+// bruteforce but (*1388 ms*)[ACCEPTED]
+class Solution {
+public int maxProduct(String[] words) {
+int max= 0,l = words.length;
+Arrays.sort(words,(a,b)->b.length()-a.length());
+    for(int i=0;i<l-1;i++){
+        for(int j=i+1;j<l;j++){
+            int f = 0;
+            for(int k=0;k<words[j].length();k++){
+                if(words[i].contains(""+words[j].charAt(k))){
+                    f=1;break;
+                }
+            }
+            if(f==0){
+                max = Math.max(max,words[i].length()*words[j].length());
+                break;
+            }
+        }
+    }
+    return max;
+}
+}
+
+
 //approach 1 :using regex[TLE]
 class Solution {
     public int maxProduct(String[] words) {
@@ -7,7 +31,6 @@ class Solution {
       for(int i=0;i<words.length;i++){
           for(int j=i+1;j<words.length;j++){
               if(words[i].matches("[^"+words[j]+"]+")){
-                  // System.out.print(words[i].matches("[^"+words[j]+"]+")+" ");
                   max=Math.max(max,words[i].length()*words[j].length());
               }
           }
