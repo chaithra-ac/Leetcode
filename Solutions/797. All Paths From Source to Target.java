af87b78ba82b524f4cd88a34786481b9db46471e
tricky_coder <61200794+chaithra-ac@users.noreply.github.com>
2022-08-09 22:11:50 +0530

    Create 797. All Paths From Source to Target.java

---
+++ Solutions/797. All Paths From Source to Target.java
@@ -0,0 +1,21 @@
+//https://leetcode.com/problems/all-paths-from-source-to-target/
+
+class Solution {
+      List<List<Integer>>list=new ArrayList<>();
+    public List<List<Integer>> allPathsSourceTarget(int[][] graph) {
+        List<Integer>l=new ArrayList<>();
+        dfs(0,graph,l);
+        return list;
+    }
+    void dfs(int cur,int[][]g,List<Integer>l){
+        l.add(cur);
+        if(cur==g.length-1){
+            list.add(new ArrayList(l));
+            return;
+        }
+        for(int adj:g[cur]){
+                dfs(adj,g,l);
+                l.remove(l.size()-1);
+        }  
+    }
+}
