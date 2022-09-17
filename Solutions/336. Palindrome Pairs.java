4903660ea32a26f09e100978eecdadc2d361f126
tricky_coder <61200794+chaithra-ac@users.noreply.github.com>
2022-09-17 19:23:54 +0530

    Update 336. Palindrome Pairs.java

--- Solutions/336. Palindrome Pairs.java
+++ Solutions/336. Palindrome Pairs.java
@@ -18,23 +18,25 @@ class Solution {
         return res;
     }
     boolean checkPalindrome(String concats,Map<String,Boolean>map){
-          if(map.containsKey(concats)){
+          if(map.containsKey(concats))
             return map.get(concats);
-        }
       
         StringBuilder concatr=new StringBuilder();
               int len=concats.length();
+        
               if(len%2==0)
                concatr.append(concats.substring(len/2,len)).reverse();
               else
                concatr.append(concats.substring(len/2+1,len)).reverse(); 
-            if(concats.substring(0,len/2).equals(concatr.toString())||
-                     concats.substring(0,len/2).equals(concatr.toString())){
+        
+            if(concats.substring(0,len/2).equals(concatr.toString())){
                       map.put(concats,true);
                       return true;
                   }
                   map.put(concats,false);
         return false;
  
+    }
+} 
     }
 }
